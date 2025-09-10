b5953dd4198715955c3167f6ff3fc576b3dd41c6
DeSheans <desheans@yandex.ru>
2025-09-10 18:56:44 +0300

    fix: changed random distribution

--- pkg/mult.go
+++ pkg/mult.go
@@ -4,7 +4,7 @@ import "math/rand/v2"
 
 func Mult(rtp float64) float64 {
 	if rand.Float64() < rtp {
-		return 10_000.0
+		return 9000 + rand.NormFloat64()*1000
 	}
-	return 1.0
+	return 1.0 + rand.NormFloat64()*0.5*1000
 }
